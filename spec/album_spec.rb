34e21e7966e9d12e00b7010be1994328a81ba618
Staff <epicodus_staff@staff.local>
2019-05-08 11:57:56 -0700

    add save and clear functionality

---
+++ spec/album_spec.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+require 'album'
+
+describe '#Album' do
+  before(:each) do
+    Album.clear
+  end
+
+  describe '.clear' do
+    it 'clears all albums' do
+        album = Album.new('Giant Steps', nil)
+        album.save
+        album2 = Album.new('Blue', nil)
+        album2.save
+        Album.clear
+        expect(Album.all).to(eq([]))
+      end
+    end
+
+  describe '#save' do
+    it 'saves an album' do
+      album = Album.new('Giant Steps', nil)
+      album.save
+      album2 = Album.new('Blue', nil)
+      album2.save
+      expect(Album.all).to(eq([album, album2]))
+    end
+  end
+
+  describe '.all' do
+    it('returns an empty array when there are no albums') do
+      expect(Album.all).to(eq([]))
+    end
+  end
+
+  describe '#==' do
+    it('is the same album if it has the same attributes as another album') do
+      album = Album.new('Blue', nil)
+      album2 = Album.new('Blue', nil)
+      expect(album).to(eq(album2))
+    end
+  end
+end
